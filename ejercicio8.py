4b20fa13fe7eca7c7ea650f353090a99dbd8104d
zaray531 <zaraym531@gmail.com>
2026-03-06 14:58:47 -0500

    Update ejercicio8.py

--- ejercicio8.py
+++ ejercicio8.py
@@ -1,7 +1,5 @@
-capital=float(input("Escribe tu capital:"))
-total = capital + (capital * 0.02)
-ganancia = capital * 0.02
-
-print("La ganancia en un mes es de:" , total)
-
-print("La capital total en un mes es de:" , ganancia)
+capital=float(input("Escribe tu capital: "))
+ganancia=capital*0.02
+total=capital+ganancia
+print("La ganancia es:", ganancia)
+print("El total es:", total)
