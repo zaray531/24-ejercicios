9abe3b6a223d913ea8c94a7ec0f1f7555da5eab5
zaray531 <zaraym531@gmail.com>
2026-03-06 14:57:07 -0500

    Update ejercicio7.py

---
+++ ejercicio7.py
@@ -0,0 +1,6 @@
+horas=int(input("horas:"))
+minutos=int(input("minutos:"))
+segundos=int(input("segundos:"))
+
+total_segundos=horas*3600+minutos*60+segundos
+print("El tiempo en segundos es:", total_segundos)
