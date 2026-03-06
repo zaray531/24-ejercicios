a9f4f29e425831ae03efe72f65aff7ecbb272863
zaray531 <zaraym531@gmail.com>
2026-03-06 15:05:09 -0500

    Update ejercicio18.py

---
+++ ejercicio18.py
@@ -0,0 +1,9 @@
+matricula=float(input("Ingrese el valor de la matrícula: "))
+cuota_1=matricula*0.40
+cuota_2=matricula*0.25
+cuota_3=matricula*0.20
+cuota_4=matricula*0.15
+print("Cuota 1:", cuota_1)
+print("Cuota 2:", cuota_2)
+print("Cuota 3:", cuota_3)
+print("Cuota 4:", cuota_4)
