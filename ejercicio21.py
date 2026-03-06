d5e681f7cdb646414839094421a745fbd8f2b72c
zaray531 <zaraym531@gmail.com>
2026-03-06 15:07:29 -0500

    Update ejercicio21.py

---
+++ ejercicio21.py
@@ -0,0 +1,8 @@
+nombre=input(" nombre: ")
+direccion=input("direccion: ")
+anio_nacimiento=int(input("año de nacimiento: "))
+
+edad=2026-anio_nacimiento
+print("Nombre:", nombre)
+print("Dirección:", direccion)
+print("Edad:", edad)
