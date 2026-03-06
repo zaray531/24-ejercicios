e2707b6969200f195cf0e0b50a5f31d1a94ce3ee
zaray531 <zaraym531@gmail.com>
2026-03-06 15:00:50 -0500

    Update ejercicio9.py

--- ejercicio9.py
+++ ejercicio9.py
@@ -1,5 +1,8 @@
-salario_base=float(input("Ingrese su salario base: "))
-comision = salario_base * 0.10
-comision_final = comision * 3
-print("La comisión que obtendrá por las tres ventas del mes será de:",comision_final)
-print("El total del salario incluyendo las comisiones es de:", salario_base + comision_final)
+sueldo_base=float(input("Ingrese el sueldo base: "))
+v1=float(input("venta 1:"))
+v2=float(input("venta 2:"))
+v3=float(input("venta 3:")) 
+comisiones=(v1+v2+v3)*0.10
+total=comisiones+sueldo_base
+print("Las comisiones son:", comisiones)
+print("El sueldo total es:", total)
