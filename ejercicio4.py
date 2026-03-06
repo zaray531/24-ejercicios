64c0c3193ad814fa3916054e3301ae3302690fb7
zaray531 <zaraym531@gmail.com>
2026-03-06 14:55:13 -0500

    Update ejercicio4.py

---
+++ ejercicio4.py
@@ -0,0 +1,5 @@
+nota_1=float(input("nota 1:"))
+nota_2=float(input("nota 2:"))
+nota_3=float(input("nota 3:"))  
+definitiva=nota_1*0.20+nota_2*0.30+nota_3*0.50
+print("La nota definitiva es:", definitiva)
