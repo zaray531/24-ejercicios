7a668bd6d9ecb287cd809e7533fe3902a6069a25
zaray531 <zaraym531@gmail.com>
2026-03-06 15:02:54 -0500

    Update ejercicio12.py

---
+++ ejercicio12.py
@@ -0,0 +1,5 @@
+hombre=int(input("Cantidad de hombres: "))
+mujeres=int(input("Cantidad de mujeres: "))
+total_personas=hombre+mujeres
+print("Porcentaje de hombres:", hombre*100/total_personas,"%")
+print("Porcentaje de mujeres:", mujeres*100/total_personas,"%")
