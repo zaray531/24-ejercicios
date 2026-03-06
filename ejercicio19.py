fd05eedc30e5c64542f05616ef529976a820a9d8
zaray531 <zaraym531@gmail.com>
2026-03-06 15:06:44 -0500

    Update ejercicio19.py

---
+++ ejercicio19.py
@@ -0,0 +1,15 @@
+nombre=input("nombre:")
+programa=input("programa:")
+ficha=input("ficha:")
+
+suma=0
+suma=suma+float(input("nota 1: "))
+suma=suma+float(input("nota 2: "))
+suma=suma+float(input("nota 3: "))
+suma=suma+float(input("nota 4: "))
+suma=suma+float(input("nota 5: "))
+promedio=suma/5
+print("Nombre:", nombre)
+print("Programa:", programa)
+print("Ficha:", ficha)
+print("Promedio:", promedio)
